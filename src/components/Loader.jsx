c2b09785bef542b4df37942930de664a7aac04ae
glafay <guillaumel1103@gmail.com>
2025-01-09 15:39:27 +0100

    COmmit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,31 +1,21 @@
-import { Html, useProgress } from "@react-three/drei";
-
-const CanvasLoader = () => {
+import React from "react";
+import { Html, UseProgress } from "@react-three/drei";
+export default function Loader() {
   const { progress } = useProgress();
-  return (
-    <Html
-      as='div'
-      center
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <span className='canvas-loader'></span>
+  rbeturn (
+    <Html>
+      <span className="canvas-loader"></span>
       <p
         style={{
           fontSize: 14,
-          color: "#F1F1F1",
-          fontWeight: 800,
-          marginTop: 40,
+          color:#f1f1f1,
+          fontWeight: 800, 
+          marginTop: 40
         }}
       >
+        {" "}
         {progress.toFixed(2)}%
       </p>
     </Html>
   );
-};
-
-export default CanvasLoader;
+}
---
+++ src/components/canvas/Computer.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControl, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
+export default function Computer({ isMobile }) {
+  const computer = useGLTF("../../../public/desktop_pc/scene.gltf");
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <spotlight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive>
+        object={computer.scene}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.2]}
+      </primitive>
+    </mesh>
+  );
+}
+
+const computersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQeuryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleMediaQeuryChange);
+    return () => {
+      mediaQuery.addEventListener("change", handleMediaQeuryChange);
+    };
+  }, [third]);
+  return (
+    <Canvas
+      frameLoop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControl
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngke={Math.PI / 2}
+        ></OrbitControl>
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
